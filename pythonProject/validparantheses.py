401f6fab14d1d4df2e4a24e505f95425abd2e3f2
suhail01101984 <suhailmemon84@gmail.com>
2023-09-29 17:04:30 -0600

    longestcommonprefix.py

---
+++ pythonProject/validparantheses.py
@@ -0,0 +1,22 @@
+#https://leetcode.com/problems/valid-parentheses/submissions/
+class Solution:
+    def isValid(self, s:str) -> bool:
+        stack=[]
+        pairs={"(":")",
+               "{":"}",
+               "[":"]"}
+        break_ind=0
+        for bracket in s:
+            if bracket in pairs:
+                stack.append(bracket)
+            elif stack==[] or bracket!=pairs[stack.pop()]:
+                break_ind=1
+
+        if(len(stack)==0) and break_ind==0:
+            return True
+        else:
+            return False
+
+
+s=Solution()
+print(s.isValid("]"))
